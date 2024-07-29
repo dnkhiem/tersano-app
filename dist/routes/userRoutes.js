70fbe97c337ea116f95e3f26bc6a255ae5cab7b1
dnkhiem <dnkhiem@gmail.com>
2024-07-28 20:17:18 -0400

    update vercel

--- dist/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var userController_1 = require("../controllers/userController");
-var router = (0, express_1.Router)();
-router.post('/register', userController_1.register);
-router.post('/login', userController_1.login);
-exports.default = router;
